a78589a7c4150e79244b8529d29a8c718f8f0e49
kayano sho <kayano@beans.jp>
2018-09-20 20:24:42 +0900

    課題を提出します。

---
+++ Assets/Scenes/kadai.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kadai : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        // 要素数5の配列を初期化する
+        int[] kadai = new int[5];
+
+        // 配列の各要素に値を代入する
+        kadai[0] = 11;
+        kadai[1] = 22;
+        kadai[2] = 33;
+        kadai[3] = 44;
+        kadai[4] = 55;
+
+        // 配列の各要素の値を順番に表示する
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(kadai[i]);
+        }
+
+        // 配列の各要素の値を逆順に表示する
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(kadai[i]);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scenes/kadai02.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    private int mp = 53; // 魔法力
+
+    // 魔法用の関数
+    public void Magic(int Magic)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            // 残りmpを減らす
+            this.mp -= Magic;
+
+            Debug.Log("魔法攻撃をした。残りMPは" + this.mp + "。");
+
+            if (this.mp < Magic)
+            {
+
+                Debug.Log("MPが足りないため魔法が使えない。");
+
+            }
+        }
+    }
+}
+
+
+public class kadai02 : MonoBehaviour
+{
+
+    void Start()
+    {
+        // Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+
+        // 魔法用の関数を呼び出す
+        lastboss.Magic(5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
